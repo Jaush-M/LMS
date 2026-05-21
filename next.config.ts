943d0775e713ff974d095a7cd85fe220862337ef
Jaush <jaushvx@icloud.com>
2026-05-21 05:22:15 +0500

    chore: hide devindicators

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [{ source: "/", destination: "/dashboard", permanent: false }];
   },
+  devIndicators: false,
 };
 
 export default nextConfig;
